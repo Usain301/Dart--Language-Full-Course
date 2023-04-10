d2219f7ebfcdc20a820cce053aa8fcd0634b40e0
Usain301 <73827957+Usain301@users.noreply.github.com>
2023-04-10 23:29:37 +0300

    tam gaz devammmm

---
+++ week3/methods.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  int Usermoney2 = 5;
+  int minimumMoney = 5;
+
+  controlUserMoney(Usermoney2, minimumMoney);
+}
+
+void controlUserMoney(int money, int minMoney) {
+  if (money > minMoney) {
+    print("bankaya girebilirsin");
+  } else {
+    print("siktir git");
+  }
+}
