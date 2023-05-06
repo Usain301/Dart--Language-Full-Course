c038a6cf4b4ffe71ae60264c06da7122f920e836
Usain301 <73827957+Usain301@users.noreply.github.com>
2023-05-06 22:06:13 +0300

    Update Enum.dart

--- week4/Enum.dart
+++ week4/Enum.dart
@@ -1,3 +1,21 @@
 void main(List<String> args) {
-  
+  final customerMouse = Mouses.a4;
+  print(customer)
+  switch (customerMouse) {
+    
+    case Mouses.magic:
+      // TODO: Handle this case.
+      break;
+    case Mouses.apple:
+      // TODO: Handle this case.
+      break;
+    case Mouses.logitech:
+      // TODO: Handle this case.
+      break;
+    case Mouses.a4:
+      // TODO: Handle this case.
+      break;
+  }
 }
+
+enum Mouses { magic, apple, logitech, a4 }
