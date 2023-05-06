ba2f4398f387c6a451879f598ce3c0e3ba31d960
Usain301 <73827957+Usain301@users.noreply.github.com>
2023-05-06 19:46:12 +0300

    new commit

--- week4/class_extend.dart
+++ week4/class_extend.dart
@@ -1,25 +1,45 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  final userNormal = User("hüseyin", 20000);
+  final usersBank = BankUser("ahmet", 10, 23);
+  final userSpecial = SpecialUser("name", 23214, 1, 214);
+  userNormal.sayCompanyUserName();
+}
 
-class User {
+//benim paralarını görmelerini istiyorum.banka ismimle beraber yazsın
+abstract class IUser {
   final String name;
   final int money;
 
-  User(this.name, this.money);
+  IUser(this.name, this.money);
+  void sayCompanyUserName() {
+    print("ahmet -$money paranız vardır");
+  }
 }
 
-class BankUser {
+class User extends IUser {
   final String name;
-  final String money;
-  final int bankingCode;
+  final int money;
 
-  BankUser(this.name, this.money, this.bankingCode);
+  User(this.name, this.money) : super(name, money);
 }
 
-class SpecialUser {
-  final String name;
-  final String money;
+class BankUser extends IUser {
   final int bankingCode;
-  final int disccount;
 
-  SpecialUser(this.name, this.money, this.bankingCode, this.disccount);
+  BankUser(String name, int money, this.bankingCode) : super(name, money);
+}
+
+class SpecialUser extends IUser {
+  final int bankingCode;
+  late final int _disccount;
+
+  SpecialUser(String name, int money, this.bankingCode, int disccount)
+      : super(name, money) {
+    _disccount = disccount; 
+  }
+
+  //indirimli faiytın ne kadar
+  int calculateNMoney() {
+    return money - (money ~/ _disccount);
+  }
 }
