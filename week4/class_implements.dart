2033730ed51fd85243134e3d5f26b1c294e03461
Usain301 <73827957+Usain301@users.noreply.github.com>
2023-05-06 21:56:09 +0300

    summary

--- week4/class_implements.dart
+++ week4/class_implements.dart
@@ -1,3 +1,35 @@
-void main(List<String> args) {
-  
+void main(List<String> args) {}
+
+abstract class IStudent {
+  final String name;
+  final String age;
+
+  IStudent(this.name, this.age);
+
+  void saySomething() {
+    print(age);
+  }
+}
+
+class Student implements IStudent {
+  final String name;
+  final String age;
+  Student(this.name, this.age);
+
+  @override
+  void saySomething() {
+  @override
+   int? schoolID;
+  }
+}
+
+class Student2 implements IStudent {
+  final String name;
+  final String age;
+  Student2(this.name, this.age);
+
+  @override
+  void saySomething() {
+    // TODO: implement saySomething
+  }
 }
