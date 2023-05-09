0a5f7c30a9eda2a05491c48cc62791c5db73126e
Usain301 <73827957+Usain301@users.noreply.github.com>
2023-05-09 15:17:40 +0300

    Update String_extension.dart

--- week4/String_extension.dart
+++ week4/String_extension.dart
@@ -1,13 +1,12 @@
 void main(List<String> args) {
-  if("admin".isAdmin()){
+  if ("admin".isAdmin()) {
     print("object");
-
   }
 }
+//
 
-extension isStringCheckExtension on String{
-
-  bool isAdmin(){
-    return this =="admin";
+extension isStringCheckExtension on String {
+  bool isAdmin() {
+    return this == "admin";
   }
 }
